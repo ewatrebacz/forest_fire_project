78b73c49ad12dd35902825df852df8c58d41f30b
ZiveRael <papeernscal@gmail.com>
2025-06-17 19:27:16 +0200

    Init A-SIS report

---
+++ script/asis.py
@@ -0,0 +1,22 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.17.0
+#   kernelspec:
+#     display_name: A-SIS
+#     language: python
+#     name: a-sis
+# ---
+
+# %% [markdown]
+# # A-SIS implementation
+
+# %%
+import networkx as nx
+import numpy as np
+
+# %%
